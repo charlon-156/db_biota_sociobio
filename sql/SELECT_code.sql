86bf4b8fa3cf6daa58c8418a14597049791bbe38
Charlon Fernandes <charlon135@gmail.com>
2025-10-01 16:10:52 -0300

    atualização do Select frente novos dados

--- sql/SELECT_code.sql
+++ sql/SELECT_code.sql
@@ -1,6 +1,18 @@
-SELECT pp.resourceID, pp.title, pp.description, t.TYPE AS tipo, i.institution AS instituicao, pp.bibliographicCitation, pp.references_url FROM public_policies pp
+-- -------------------------------------------------------------
+-- Dev.: Charlon F. Monteiro
+-- project: Banco de Dados Sociobiodiversidade
+-- file: base.py
+-- description: Important module responsible for centralizing 
+--              functions to support the generation of SQL 
+--              commands for insertion from Excel spreadsheets
+-- Last update: 2025-09-29
+-- --------------------------------------------------------------
+
+
+SELECT pp.resourceID, pp.title, pp.description, l.legislativeStatus AS 'Status da legislação', t.TYPE AS tipo, i.institution AS instituicao, pp.bibliographicCitation, pp.references_url FROM public_policies pp
 JOIN type_pp t ON pp.typeID = t.typeID
 JOIN institutions i ON pp.institutionID = i.institutionID
+LEFT JOIN legislativestatus l ON pp.legislativeStatusID = l.legislativeStatusID  
 ORDER BY pp.resourceID;
 
 SELECT m.municipalityID AS 'Código do município', m.municipality AS 'Município', gi.rgi AS 'Região Imediata', g.rgint AS 'Região Intermédiarias', m.areaKM2 AS 'Área KM²', m.population AS 'População', m.man AS 'Qntd. Homens', m.woman AS 'Qntd. Mulheres', m.genderRatio AS 'Razão de gênero', m.middleAge AS 'Idade média', m.populationDensity AS 'Densidade populacional', m.indigenousPopulation AS 'população indigena', m.insideIndigenousLand AS 'indigena dentro de território', m.outsideIndigenousLand AS 'indigena fora de território', m.quilombolaPopulation AS 'população quilombola', m.insideQuilombolaLand AS 'quilombola dentro de território', m.outsideQuilombolaLand AS 'quilombola fora de território', m.populationByRaceAmarela AS 'Qntd. de Amarelos', m.populationByRaceBranca AS 'Qntd. de Brancos', m.populationByRaceIndigena AS 'Qntd. de Indigenas', m.populationByRaceParda AS 'Qntd. de Pardos', m.populationByRacePreta AS 'Qntd. de Pretos' FROM municipalities m 

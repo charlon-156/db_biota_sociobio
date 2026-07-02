ad83cb84be8ae87b9ddfc77f7c9a9405b60e037e
Charlon Fernandes <charlon135@gmail.com>
2026-07-02 20:40:26 -0300

    atualização de dados

---
+++ result.sh
@@ -0,0 +1,10 @@
+#!usr/bin/bash
+python insert_climate-mun.py
+python insert_mun-ugrhi.py
+python insert_municipality.py
+python insert_pp-fk.py
+python insert_pp-municipality.py
+python insert_pp-species.py
+python insert_pp.py
+python insert_species-fk.py
+python insert_species.py
